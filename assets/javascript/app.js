bacf67ee982ed40f9f98474a36c8688a3d0c199e
Chloe Kitts <47789998+chloeekitts@users.noreply.github.com>
2019-04-15 07:55:18 -0500

    Add files via upload
    Updates to css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 var topics = [];
 
 	//Function with AJAX call to GIPHY; Q parameterc for API link set to search term, limit 10 results
-  //Create div with respective still and animate image sources with "data-state", "data-still" and "data-animate" attributes
  	function displayNetflixShow() {
 
 	var x = $(this).data("search");
